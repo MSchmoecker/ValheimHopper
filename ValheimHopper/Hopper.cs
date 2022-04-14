6e984c56cd6efabc2b062cc9d56286180de30a66
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2022-04-14 14:16:36 +0200

    added hopper item movement into smelter

--- ValheimHopper/Hopper.cs
+++ ValheimHopper/Hopper.cs
@@ -35,23 +35,20 @@ namespace ValheimHopper {
             }
 
             bool pushed = PushItemsIntoChests();
+
+            if (!pushed) {
+                PushItemsIntoSmelter();
+            }
         }
 
         private bool PushItemsIntoChests() {
             List<Container> chestsTo = FindContainer(new Vector3(0, -0.25f * 1.5f, 0), false);
 
             foreach (Container to in chestsTo) {
-                bool pushed = IterateInventory(selfContainer.GetInventory(), firstItem => {
-                    if (to.GetInventory().CanAddItem(firstItem, 1)) {
-                        Vector2i gridPos = firstItem.m_gridPos;
-                        to.AddItemToChest(selfContainer, gridPos, new Vector2i(-1, -1));
-                        return true;
-                    }
+                ItemDrop.ItemData item = selfContainer.GetInventory().FindFirstItem(i => to.GetInventory().CanAddItem(i, 1));
 
-                    return false;
-                });
-
-                if (pushed) {
+                if (item != null) {
+                    to.AddItemToChest(selfContainer, item.m_gridPos, new Vector2i(-1, -1));
                     return true;
                 }
             }
@@ -63,17 +60,10 @@ namespace ValheimHopper {
             List<Container> chestsFrom = FindContainer(new Vector3(0, 0.25f * 1.5f, 0), true);
 
             foreach (Container from in chestsFrom) {
-                bool movedItem = IterateInventory(from.GetInventory(), firstItem => {
-                    if (selfContainer.GetInventory().CanAddItem(firstItem, 1)) {
-                        Vector2i gridPos = firstItem.m_gridPos;
-                        from.RemoveItemFromChest(selfContainer, gridPos, new Vector2i(-1, -1));
-                        return true;
-                    }
+                ItemDrop.ItemData item = from.GetInventory().FindFirstItem(i => selfContainer.GetInventory().CanAddItem(i, 1));
 
-                    return false;
-                });
-
-                if (movedItem) {
+                if (item != null) {
+                    from.RemoveItemFromChest(selfContainer, item.m_gridPos, new Vector2i(-1, -1));
                     return true;
                 }
             }
@@ -81,21 +71,29 @@ namespace ValheimHopper {
             return false;
         }
 
-        private List<ItemDrop> FindItemDrops(Vector3 relativePos) {
-            Vector3 center = transform.position + relativePos;
-            int count = Physics.OverlapBoxNonAlloc(center, Vector3.one / 2f, tmpColliders, Quaternion.identity, itemMask);
+        private void PushItemsIntoSmelter() {
+            List<Smelter> smelters = FindSmelters(new Vector3(0, -0.25f * 1.5f, 0));
 
-            List<ItemDrop> items = new List<ItemDrop>();
+            foreach (Smelter smelter in smelters) {
+                ItemDrop.ItemData item = selfContainer.GetInventory().FindFirstItem(i => {
+                    bool isAllowedOre = smelter.IsItemAllowed(i) && smelter.GetQueueSize() < smelter.m_maxOre;
+                    bool isFuelItem = smelter.m_fuelItem.m_itemData.m_shared.m_name == i.m_shared.m_name;
+                    bool isAllowedFuel = isFuelItem && smelter.GetFuel() < smelter.m_maxFuel - 1;
+                    return isAllowedOre || isAllowedFuel;
+                });
 
-            for (int i = 0; i < count; i++) {
-                ItemDrop item = tmpColliders[i].gameObject.GetComponentInParent<ItemDrop>();
+                if (item != null) {
+                    selfContainer.GetInventory().RemoveItem(item, 1);
 
-                if (item) {
-                    items.Add(item);
+                    if (smelter.IsItemAllowed(item)) {
+                        smelter.m_nview.InvokeRPC("AddOre", item.m_dropPrefab.name);
+                    } else {
+                        smelter.m_nview.InvokeRPC("AddFuel");
+                    }
+
+                    break;
                 }
             }
-
-            return items;
         }
 
         private bool PickupItems() {
@@ -133,13 +131,30 @@ namespace ValheimHopper {
             return false;
         }
 
+        private List<ItemDrop> FindItemDrops(Vector3 relativePos) {
+            Vector3 center = transform.position + relativePos;
+            int count = Physics.OverlapBoxNonAlloc(center, Vector3.one / 2f, tmpColliders, Quaternion.identity, itemMask);
+
+            List<ItemDrop> items = new List<ItemDrop>();
+
+            for (int i = 0; i < count; i++) {
+                ItemDrop item = tmpColliders[i].GetComponentInParent<ItemDrop>();
+
+                if (item) {
+                    items.Add(item);
+                }
+            }
+
+            return items;
+        }
+
         private List<Container> FindContainer(Vector3 relativePos, bool allowHopper) {
             Vector3 center = transform.position + relativePos;
             int count = Physics.OverlapBoxNonAlloc(center, Vector3.one / 2f, tmpColliders, Quaternion.identity, pieceMask);
             List<Container> chests = new List<Container>();
 
             for (int i = 0; i < count; i++) {
-                Container container = tmpColliders[i].gameObject.GetComponentInParent<Container>();
+                Container container = tmpColliders[i].GetComponentInParent<Container>();
 
                 if (!container || container.gameObject == gameObject) {
                     continue;
@@ -155,24 +170,20 @@ namespace ValheimHopper {
             return chests;
         }
 
-        private static bool IterateInventory(Inventory target, Func<ItemDrop.ItemData, bool> firstItem) {
-            for (int y = 0; y < target.m_height; y++) {
-                for (int x = 0; x < target.m_width; x++) {
-                    ItemDrop.ItemData item = target.GetItemAt(x, y);
+        private List<Smelter> FindSmelters(Vector3 relativePos) {
+            Vector3 center = transform.position + relativePos;
+            int count = Physics.OverlapBoxNonAlloc(center, Vector3.one / 2f, tmpColliders, Quaternion.identity, pieceMask);
+            List<Smelter> smelters = new List<Smelter>();
 
-                    if (item == null) {
-                        continue;
-                    }
+            for (int i = 0; i < count; i++) {
+                Smelter smelter = tmpColliders[i].GetComponentInParent<Smelter>();
 
-                    bool used = firstItem(item);
-
-                    if (used) {
-                        return true;
-                    }
+                if (smelter) {
+                    smelters.Add(smelter);
                 }
             }
 
-            return false;
+            return smelters;
         }
     }
 }
---
+++ ValheimHopper/InventoryHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ValheimHopper {
+    public static class InventoryHelper {
+        public static ItemDrop.ItemData FindFirstItem(this Inventory target, Func<ItemDrop.ItemData, bool> predicate) {
+            for (int y = 0; y < target.m_height; y++) {
+                for (int x = 0; x < target.m_width; x++) {
+                    ItemDrop.ItemData item = target.GetItemAt(x, y);
+
+                    if (item != null && predicate(item)) {
+                        return item;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
