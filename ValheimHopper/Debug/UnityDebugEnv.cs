8632fcfa9662daa4ed2383447356085fd8681ca5
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-06-15 16:28:09 +0200

    chore: internal fixes for Valheim 0.216.9

--- ValheimHopper/Debug/UnityDebugEnv.cs
+++ ValheimHopper/Debug/UnityDebugEnv.cs
@@ -5,24 +5,11 @@ namespace ValheimHopper.Debug {
     // [DefaultExecutionOrder(-97)]
     public class UnityDebugEnv : MonoBehaviour {
         private void Awake() {
-            ZNet.SetServer(true, false, false, "Server Name", "Test", GetDevWorld());
+            ZNet.SetServer(true, false, false, "Server Name", "Test", World.GetDevWorld());
             ZNet.instance.m_players.Add(new ZNet.PlayerInfo());
             Game.instance.m_playerProfile = new PlayerProfile("Developer", FileHelpers.FileSource.Local);
             Game.instance.m_playerProfile.SetName("Odev");
             Game.instance.m_playerProfile.Load();
         }
-
-        public static World GetDevWorld() {
-            World devWorld = World.LoadWorld("DevWorld", FileHelpers.FileSource.Local);
-
-            if (devWorld.m_dataError == World.SaveDataError.None) {
-                return devWorld;
-            }
-
-            World devWorld2 = new World("DevWorld", "DevWorldSeed");
-            devWorld2.SaveWorldMetaData(DateTime.Now);
-            return devWorld2;
-        }
-
     }
 }
